86ea4da34ea448a44ef484bb9ca59f5612c0d5ee
potatoSalad21 <datuna.ziraqishvili@gmail.com>
2023-07-05 19:21:54 +0400

    displaying weekly forecast

--- src/components/WeekDay.jsx
+++ src/components/WeekDay.jsx
@@ -4,17 +4,27 @@ export default function WeekDay({ forecastForDay }) {
     "Mon",
     "Tue",
     "Wed",
-    "Thus",
+    "Thu",
     "Fri",
     "Sat",
   ];
-  console.dir(forecastForDay);
+  
+  console.dir(forecastForDay)
   let date = new Date(forecastForDay.Date);
-  let day = weekDays[date.getDay()];
+  const day = weekDays[date.getDay()];
+  
+  const dayPhrase = forecastForDay.Day.IconPhrase;
+
+  let maxTemp = Math.round((forecastForDay.Temperature.Maximum.Value - 32) * 5 / 9);
+  let minTemp = Math.round((forecastForDay.Temperature.Minimum.Value - 32) * 5 / 9);
 
   return (
     <li className="weekDay">
-      {day}
+      <div className="info">
+        <span>{day}</span>
+        <div className="temp">{maxTemp}°/{minTemp}°</div>
+      </div>
+      <span className="phrase">{dayPhrase}</span>
     </li>
   );
 }
